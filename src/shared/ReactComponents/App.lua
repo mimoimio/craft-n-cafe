65cbd7fc3bcc7684ae30d77147f13bb0b0379b7e
mimoimio <129141433+mimoimio@users.noreply.github.com>
2026-09-19 17:52:59 +0800

    a

--- src/shared/ReactComponents/App.lua
+++ src/shared/ReactComponents/App.lua
@@ -3,9 +3,15 @@ local e = React.createElement
 
 local Main = require(script.Parent.Main)
 local ToastProvider = require(script.Parent.Toasts).ToastProvider
--- game:GetService("StarterGui"):SetCoreGuiEnabled(Enum.CoreGuiType.Backpack, false)
+local RedDotProvider = require(script.Parent.RedDot).RedDotProvider
+
 local function App(props)
-	return e(ToastProvider, {}, { e(Main) })
+	return e(ToastProvider, nil, {
+		-- Assign specific dictionary keys to ensure children pass down properly
+		AppRedDotProvider = e(RedDotProvider, nil, {
+			AppMain = e(Main),
+		}),
+	})
 end
 
 return App
